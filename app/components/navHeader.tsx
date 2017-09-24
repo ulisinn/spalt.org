cec8d1b64401bb917f7b32a39594e5dc6369e3b1
Ulrich Sinn <uli@addtothenoise.com>
2017-09-24 16:31:53 -0700

    nav header fixed for phone

--- app/components/navHeader.tsx
+++ app/components/navHeader.tsx
@@ -9,7 +9,7 @@ interface NavHeaderProps {
 }
 
 const NavHeader: React.SFC<NavHeaderProps> = ({toggleHamburger, isOpen}) => {
-    const label = (isOpen) ? `${String.fromCharCode(215)} ${String.fromCharCode(160)}` : `${String.fromCharCode(8801)} ${String.fromCharCode(160)}`;
+    const label = (isOpen) ? `${String.fromCharCode(215)} ${String.fromCharCode(160)} ${String.fromCharCode(160)}` : `${String.fromCharCode(8801)} ${String.fromCharCode(160)} ${String.fromCharCode(160)}`;
     return (
         <div className={styles.navButton} onClick={() => toggleHamburger()} style={{cursor: 'pointer'}}>
             <p>{label}</p>
